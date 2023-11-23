f5ff394adb1f1cf2a6d2119dcafa438ef3a17428
LahceneDer <63024996+LahceneDer@users.noreply.github.com>
2023-11-23 15:26:43 +0100

    Update movie.service.ts

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -17,22 +17,22 @@ export class MovieService {
     limit: number,
     skip: number
   ): Promise<IMovie[] | null> {
-    const movies = this.movieModel.find().skip(skip).limit(limit);
+    const movies = await this.movieModel.find().skip(skip).limit(limit);
     return movies;
   }
 
   public async getMovie(id: Types.ObjectId): Promise<IMovie | null> {
-    const movie = this.movieModel.findById(id);
+    const movie = await this.movieModel.findById(id);
     return movie;
   }
 
   public async getMovieTrailer(id: Types.ObjectId): Promise<IMovie | null> {
-    const movie = this.movieModel.findById(id, { _id: 0, trailerUrl: 1 });
+    const movie = await this.movieModel.findById(id, { _id: 0, trailerUrl: 1 });
     return movie;
   }
 
   public async getTop5Movies(): Promise<IMovie[] | null> {
-    const movies = this.movieModel.find().sort({ rating: -1 }).limit(5);
+    const movies = await this.movieModel.find().sort({ rating: -1 }).limit(5);
     return movies;
   }
 
